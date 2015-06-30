c32347859a34d8e516916632fc15ca0aee598b58
Diana Liao <diana.liao@mongodb.com>
2015-06-30 17:23:31 -0400

    Update test code for integration testing

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,5 @@
-// janky script to run the server and x clients
+// node script to run the proxy server and Javascript integration tests.
+// Usage: node test <directory with tests>
 
 var childProcess = require('child_process');
 var fs = require('fs');
@@ -6,7 +7,7 @@ var path = process.cwd();
 
 var server = childProcess.spawn('go', [
 	'run',
-	'./tests/main/mongod-server.go',
+	__dirname + '/main/test-server.go',
 	'-port=' + 8000,
 	'-logLevel=' + 1
 ], {
@@ -16,9 +17,19 @@ var server = childProcess.spawn('go', [
 testFiles = fs.readdirSync(process.argv[2])
 i = 0;
 
+// hack to make sure that the server is up before running tests.
 setTimeout(function() {
 
 	var id = setInterval(function() {
+		if (!testFiles[i]) {
+			clearInterval(id)
+			return;
+		}
+		var ext = testFiles[i].split('.').pop();
+		if (ext != 'js') {
+			i++;
+			return;
+		}
 
 		console.log("Testing: " + testFiles[i])
 		var shell = childProcess.spawnSync('mongo', [

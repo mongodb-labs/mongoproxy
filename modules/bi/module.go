f4b7fde0bf1d3d6626823e06448378e18f7c9a20
Diana Liao <diana.liao@mongodb.com>
2015-07-06 15:13:48 -0400

    Refactor BI module's rules

--- modules/bi/module.go
+++ modules/bi/module.go
@@ -1,91 +1,19 @@
 package bi
 
 import (
-	"fmt"
 	"github.com/mongodbinc-interns/mongoproxy/bsonutil"
 	"github.com/mongodbinc-interns/mongoproxy/convert"
 	. "github.com/mongodbinc-interns/mongoproxy/log"
 	"github.com/mongodbinc-interns/mongoproxy/messages"
 	"github.com/mongodbinc-interns/mongoproxy/server"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 
-	"strconv"
 	"time"
 )
-
-type Rule struct {
-	OriginDatabase    string
-	OriginCollection  string
-	PrefixDatabase    string
-	PrefixCollection  string
-	TimeGranularities []string
-	ValueField        string
-	TimeField         *time.Time
-}
 
 type BIModule struct {
 	Rules []Rule
 }
 
-func createSelector(t time.Time, granularity string, valueField string) (bson.D, error) {
-	var start time.Time
-	switch granularity {
-	case "M":
-		start = time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
-	case "D":
-		start = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-	case "h":
-		start = t.Round(time.Hour)
-	case "m":
-		start = t.Round(time.Minute)
-	case "s":
-		start = t.Round(time.Second)
-	default:
-		return nil, fmt.Errorf("Not a valid time granularity")
-	}
-
-	doc := bson.D{{"start", start}, {"valueField", valueField}}
-
-	return doc, nil
-}
-
-func createUpdate(t time.Time, granularity string, value float64) (bson.D, error) {
-
-	var M int
-	var granularityField string
-
-	switch granularity {
-	case "M":
-		M = int(t.Month())
-		granularityField = "month"
-	case "D":
-		M = t.Day()
-		granularityField = "day"
-	case "h":
-		M = t.Hour()
-		granularityField = "hour"
-	case "m":
-		M = t.Minute()
-		granularityField = "minute"
-	case "s":
-		M = t.Second()
-		granularityField = "second"
-	default:
-		return nil, fmt.Errorf("Not a valid time granularity")
-	}
-
-	timeField := strconv.Itoa(M)
-
-	totalUpdate := bson.DocElem{"total", bson.D{{"$inc", value}}}
-	fieldUpdate := bson.DocElem{granularityField, bson.D{{timeField, bson.D{{"$inc", value}}}}}
-	doc := bson.D{
-		totalUpdate, fieldUpdate,
-	}
-
-	return doc, nil
-
-}
 func (b BIModule) Process(req messages.Requester, res messages.Responder,
 	next server.PipelineFunc) {
 
---
+++ modules/bi/rules.go
@@ -0,0 +1,77 @@
+package bi
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"strconv"
+	"time"
+)
+
+type Rule struct {
+	OriginDatabase    string
+	OriginCollection  string
+	PrefixDatabase    string
+	PrefixCollection  string
+	TimeGranularities []string
+	ValueField        string
+	TimeField         *time.Time
+}
+
+func createSelector(t time.Time, granularity string, valueField string) (bson.D, error) {
+	var start time.Time
+	switch granularity {
+	case "M":
+		start = time.Date(t.Year(), time.January, 1, 0, 0, 0, 0, t.Location())
+	case "D":
+		start = time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
+	case "h":
+		start = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	case "m":
+		start = t.Round(time.Hour)
+	case "s":
+		start = t.Round(time.Minute)
+	default:
+		return nil, fmt.Errorf("Not a valid time granularity")
+	}
+
+	doc := bson.D{{"start", start}, {"valueField", valueField}}
+
+	return doc, nil
+}
+
+func createUpdate(t time.Time, granularity string, value float64) (bson.D, error) {
+
+	var M int
+	var granularityField string
+
+	switch granularity {
+	case "M":
+		M = int(t.Month())
+		granularityField = "month"
+	case "D":
+		M = t.Day()
+		granularityField = "day"
+	case "h":
+		M = t.Hour()
+		granularityField = "hour"
+	case "m":
+		M = t.Minute()
+		granularityField = "minute"
+	case "s":
+		M = t.Second()
+		granularityField = "second"
+	default:
+		return nil, fmt.Errorf("Not a valid time granularity")
+	}
+
+	timeField := strconv.Itoa(M)
+
+	totalUpdate := bson.DocElem{"total", bson.D{{"$inc", value}}}
+	fieldUpdate := bson.DocElem{granularityField, bson.D{{timeField, bson.D{{"$inc", value}}}}}
+	doc := bson.D{
+		totalUpdate, fieldUpdate,
+	}
+
+	return doc, nil
+
+}
